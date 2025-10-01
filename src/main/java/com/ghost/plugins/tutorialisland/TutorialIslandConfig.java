ae1d387c4f50176918dad7a52ca3f806d773a617
Ghost <ghostheart.rs@gmail.com>
2025-10-01 10:31:20 -0400

    Removed Tut plugin for now, might reassess later.

--- src/main/java/com/ghost/plugins/tutorialisland/TutorialIslandConfig.java
+++
@@ -1,8 +0,0 @@
-package com.ghost.plugins.tutorialisland;
-
-import net.runelite.client.config.Config;
-import net.runelite.client.config.ConfigGroup;
-
-@ConfigGroup("tutorialisland")
-public interface TutorialIslandConfig extends Config {
-}
